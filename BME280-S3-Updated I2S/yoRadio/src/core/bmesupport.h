55f746912436a7c5d331b845634488f03bf4ba2f
JmanNZ <jman@jman.net.nz>
2024-11-28 08:13:01 +1300

    Add files via upload

---
+++ BME280-S3-Updated I2S/yoRadio/src/core/bmesupport.h
@@ -0,0 +1,20 @@
+#ifndef bmesupport_h
+#define bmesupport_h
+#include "options.h"
+#define BMESUPPORTED (BME_SDA!=255 && BME_SCL!=255)
+
+#if BMESUPPORTED
+//#include <Adafruit_BME280.h>
+#include "../Adafruit_BME280/Adafruit_BME280.h"
+
+class BME: public Adafruit_BME280 {
+
+  public:  
+  bool init();
+  bool status();
+  };
+	
+extern BME bme;
+#endif
+
+#endif
